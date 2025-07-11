063e9575d1f9a5ff9196ff2705d2180c03d83876
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-11 21:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(cents) {
+  let change = {
+    q: 0, // quarters
+    d: 0, // dimes
+    n: 0, // nickels
+    p: 0  // pennies
+  };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+  change.q = Math.floor(cents / 25);
+  cents %= 25;
+
+  change.d = Math.floor(cents / 10);
+  cents %= 10;
+
+  change.n = Math.floor(cents / 5);
+  cents %= 5;
+
+  change.p = cents; // remaining cents are pennies
+
+  return change;
+}
+
